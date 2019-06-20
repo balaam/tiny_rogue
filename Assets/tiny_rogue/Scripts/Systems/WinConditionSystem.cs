a8c41534801f14f55fcfe37b2e76d020776d0440
Dan <balaam@gmail.com>
2019-06-20 20:10:25 +0100

    Add a win condition

---
+++ Assets/tiny_rogue/Scripts/Systems/WinConditionSystem.cs
@@ -0,0 +1,33 @@
+using game;
+using Unity.Mathematics;
+using Unity.Entities;
+
+namespace game
+{
+    // The WinCondition detection should be finer and only trigger a specific number of events.
+    public class WinConditionSystem : TurnSystem
+    {
+        protected override void OnUpdate() { }
+        
+        public override void OnTurn(uint turnNumber)
+        {
+            int2 playerPos = new int2(0,0);
+                        
+            // The player has just moved, have they won the game?
+            Entities.WithAll<Crown>().ForEach((ref WorldCoord crownPos) =>
+            {
+                Entities.WithAll<Player>().ForEach((Entity player, ref WorldCoord coord) =>
+                {
+                    playerPos.x = coord.x;
+                    playerPos.y = coord.y;
+                });
+                
+                if (playerPos.x == crownPos.x && playerPos.y == crownPos.y)
+                {
+                    var gss = EntityManager.World.GetExistingSystem<GameStateSystem>();
+                    gss.MoveToGameWin();    
+                }
+            });
+        }
+    }
+}
