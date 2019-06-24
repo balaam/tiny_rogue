73a552d979b77d05317fffdac97040c408cfe391
Sophia Clarke <sophiaclarke93@gmail.com>
2019-06-24 12:30:05 +0200

    The slime now hurts a bit if you stand on it

---
+++ Assets/tiny_rogue/Scripts/Systems/SlimeSystem.cs
@@ -0,0 +1,40 @@
+using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace game
+{
+    // Notes:
+    // - This could be more less specific - i.e. it only handles spear traps what about other traps?
+    // - The for loop nesting should be reversed. There are probably less traps than living things generally?
+    public class SlimeSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            // Did anything with health step on a trap
+            Entities.ForEach((Entity creature, ref WorldCoord coord, ref HealthPoints hp) =>
+            {
+                int2 creaturePos = new int2(coord.x, coord.y);
+                int dmg = 0;
+                Entities.WithAll<Slime>().ForEach((Entity trap, ref WorldCoord trapCoord) =>
+                {
+                    if (creaturePos.x == trapCoord.x && creaturePos.y == trapCoord.y)
+                    {
+                        if(EntityManager.HasComponent(creature, typeof(Player)))
+                        {
+                            var log = EntityManager.World.GetExistingSystem<LogSystem>();
+                            log.AddLog("You stepped on an unmoving slime. It hurt a bit.");
+                            var gss = EntityManager.World.GetExistingSystem<GameStateSystem>();
+                            gss.TurnManager.NeedToTickTurn = true;
+                        }
+
+                        dmg = 1;
+                        //PostUpdateCommands.DestroyEntity(trap);
+                    }
+                });
+                hp.now = hp.now - dmg;
+            });
+        }
+    }
+}
