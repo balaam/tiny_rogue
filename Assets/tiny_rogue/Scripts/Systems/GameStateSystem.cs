e4e308ece2fc229e4d0d015b27a08762ce33fab6
Marc Di Luzio <marcdi@unity3d.com>
2019-06-24 15:04:01 +0200

    Move game related code out of the movement system

--- Assets/tiny_rogue/Scripts/Systems/GameStateSystem.cs
+++ Assets/tiny_rogue/Scripts/Systems/GameStateSystem.cs
@@ -257,6 +257,36 @@ namespace game
             }
         }
 
+        public void Interact(int x, int y)
+        {
+            Entities.WithAll<Stairway>().ForEach((ref WorldCoord stairCoord, ref Translation stairTrans) =>
+            {
+                if (x == stairCoord.x && y == stairCoord.y)
+                    MoveToNextLevel();
+            });
+        }
+
+        public void Wait()
+        {
+            var log = EntityManager.World.GetExistingSystem<LogSystem>();
+            log.AddLog("You wait a turn.");
+            TurnManager.NeedToTickTurn = true;
+        }
+
+        public void TryMove(Entity e, int x, int y)
+        {
+            Entities.WithNone<BlockMovement>().WithAll<Tile>().ForEach((ref WorldCoord tileCoord, ref Translation tileTrans) =>
+            {
+                // This location the player wants to move has nothing blocking them, so update their position.
+                if (tileCoord.x == x && tileCoord.y == y)
+                {
+                    EntityManager.SetComponentData(e, tileCoord);
+                    EntityManager.SetComponentData(e, tileTrans);                        
+                    TurnManager.NeedToTickTurn = true;
+                }
+            });
+        }
+        
         private void CleanUpGameWorld()
         {
             var log = EntityManager.World.GetExistingSystem<LogSystem>();
