339cf879871583bb66089ee417b0a84f95dbd437
Christopher Addison <christophera@unity3d.com>
2019-06-25 23:32:14 +0200

    Allow animations to be on NPCs

---
+++ Assets/tiny_rogue/Scripts/Components/Animated.cs
@@ -0,0 +1,20 @@
+using Unity.Entities;
+
+namespace game
+{
+    public enum Direction
+    {
+        Right = 0,
+        Down = 1,
+        Left = 2,
+        Up = 3
+    }
+    
+    public struct Animated : IComponentData
+    {
+        public Direction Direction;
+        public Action Action;
+        public bool AnimationTrigger;
+        public float AnimationTime;
+    }
+}
