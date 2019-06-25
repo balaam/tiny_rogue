d446ef6a881535b6c39fc1eaf00b8aaf5ce65574
Victor Willyams <victorw@unity3d.com>
2019-06-25 11:06:09 -0500

    restore the stuff I originally intended to merge (#58)

--- Assets/tiny_rogue/Scripts/Components/MeleeAttackMovement.cs
+++ Assets/tiny_rogue/Scripts/Components/MeleeAttackMovement.cs
@@ -3,7 +3,7 @@ using Unity.Entities;
 namespace game
 {
     // The Entity will attempt to move into Melee range to attack the player
-    public class MeleeAttackMovement : IComponentData
+    public struct MeleeAttackMovement : IComponentData
     {
         
     }
