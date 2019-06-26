68b94dfb4881d5e068d8aa6ea1769249e6eda96f
Marc Di Luzio <marcdi@unity3d.com>
2019-06-26 17:38:17 +0200

    Fix Scene Loading

--- Assets/tiny_rogue/Scripts/Systems/UI/ButtonLoadSceneSystem.cs
+++ Assets/tiny_rogue/Scripts/Systems/UI/ButtonLoadSceneSystem.cs
@@ -10,14 +10,19 @@ public class ButtonLoadSceneSystem : ComponentSystem
 {
 	protected override void OnUpdate()
 	{
+		ButtonLoadScene scene = new ButtonLoadScene();
+		bool sceneSelected = false;
 		Entities.WithAll<ButtonLoadScene>().ForEach((Entity entity, ref PointerInteraction pointerInteraction) =>
             {
                 if (pointerInteraction.clicked)
 				{
                     // Go to the referenced scene
-                    var buttonData = EntityManager.GetComponentData<ButtonLoadScene>(entity);
-                    SceneService.LoadSceneAsync(buttonData.sceneReference);
-                }
+                    scene = EntityManager.GetComponentData<ButtonLoadScene>(entity);
+                    sceneSelected = true;
+				}
             });
+		
+		if(sceneSelected)
+			SceneService.LoadSceneAsync(scene.sceneReference);
 	}
 }
