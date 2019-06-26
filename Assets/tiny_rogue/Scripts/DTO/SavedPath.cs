ae50b7d5bff6d96ae99893d66d76f7b0de086b49
Victor Willyams <victorw@unity3d.com>
2019-06-26 04:22:24 -0500

    comment out non-blittable code

--- Assets/tiny_rogue/Scripts/DTO/SavedPath.cs
+++ Assets/tiny_rogue/Scripts/DTO/SavedPath.cs
@@ -8,16 +8,7 @@ namespace game
     {
         public NativeArray<int2> pathSteps;
         public int currentIdx;
-
-        public static SavedPath GetEmpty()
-        {
-            return new SavedPath
-            {
-                pathSteps = new NativeArray<int2>(0, Allocator.Temp), // Will become deallocated, you should replace this
-                currentIdx = -1
-            };
-        }
-
+        
         public void Dispose()
         {
             pathSteps.Dispose();
