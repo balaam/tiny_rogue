905dc173a00fd2cede2fe21d7381bbff40708f3a
Marc Di Luzio <marcdi@unity3d.com>
2019-06-26 18:01:49 +0200

    Fix player health in IL2CPP build

--- Assets/tiny_rogue/Scripts/TinyRogueConstants.cs
+++ Assets/tiny_rogue/Scripts/TinyRogueConstants.cs
@@ -8,8 +8,8 @@ namespace game
         /// <summary>
         /// 1/DoorProbability or being drawn for each door in the level.
         /// </summary>
-        public static int DoorProbability = 5;
-        public static int StartPlayerHealth = 10;
+        public const int DoorProbability = 5;
+        public const int StartPlayerHealth = 10;
         public static float3 OffViewport = new float3(-9999, -9999, 0);
         public static Color DefaultColor = new Color(168.0f/255.0f, 168.0f/255.0f,168.0f/255.0f);
     }
